c31a3dbde0a092a0d086a7e65920e5801442abd5
PowerOleg <geraskin.oleg@mail.ru>
2023-03-21 20:50:24 +0500

    bean from context(xml)

--- src/main/java/com/spring/constructor/Engine.java
+++ src/main/java/com/spring/constructor/Engine.java
@@ -1,13 +1,12 @@
 package com.spring.constructor;
 
-import org.springframework.stereotype.Component;
 
 public class Engine {
     private String nameEngine = "BMW";
 
-//    public Engine(String nameEngine) {
-//        this.nameEngine = nameEngine;
-//    }
+    public Engine(String nameEngine) {
+        this.nameEngine = nameEngine;
+    }
 
     @Override
     public String toString() {
